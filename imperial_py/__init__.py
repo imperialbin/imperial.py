9ed07de38a65414452ed188fe2095279aac2ae75
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-03-24 22:32:42 -0400

    update __init__.py

--- imperial_py/__init__.py
+++ imperial_py/__init__.py
@@ -1,3 +1,4 @@
 __version__ = "1.0.3"
 __author__ = "Hexiro"
-from .imperialbin import *
+from .imperialbin import Imperial
+from .imperialbin import create_document, get_document, edit_document, verify
