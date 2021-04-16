70b08f2fdb2a5052279cab443c3e88cd6c72beb7
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-04-16 17:55:39 -0400

    bump to 2.0.0 beta 2

--- imperial_py/__init__.py
+++ imperial_py/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.0b1"
+__version__ = "2.0.0b2"
 __author__ = "Hexiro"
 from .imperialbin import Imperial
 from .imperialbin import create_document, get_document, edit_document, delete_document, verify
