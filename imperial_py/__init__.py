79c2d453860bfebe92a5702eb97db915114fe364
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-04-26 12:02:36 -0400

    explicitly typed version -> dunder version

--- imperial_py/__init__.py
+++ imperial_py/__init__.py
@@ -1,4 +1,3 @@
-__version__ = "2.0.0b2"
 __author__ = "Hexiro"
 from .imperialbin import Imperial
 from .imperialbin import create_document, get_document, edit_document, delete_document, verify
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 __author__ = "Hexiro"
 from setuptools import setup
-from imperial_py import __version__
 
 with open("README.md", encoding="utf-8") as readme_file:
     readme = readme_file.read()
@@ -10,7 +9,7 @@ with open("requirements.txt", encoding="utf-8") as req_file:
 
 setup(
     name="imperial-py",
-    version=__version__,
+    version="2.0.0b2",
     description="ImperialBin is a hastebin alternative built with UI and user experience in mind.",
     long_description=readme,
     long_description_content_type="text/markdown",
