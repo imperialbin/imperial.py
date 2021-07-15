abc96c4b4016bf084e40c296bb52f156fc975efa
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-07-15 19:30:37 -0400

    ayo! no more wildcard imports

--- imperial_py/utils/__init__.py
+++ imperial_py/utils/__init__.py
@@ -1,5 +1,5 @@
 from .hostname import https
-from .parser import *
+from .parser import ensure_json, to_snake_case, to_camel_case, parse_dates, get_date_difference
 
 # read notes from imperial_py\__init__.py
 del hostname
