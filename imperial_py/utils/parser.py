4b032aa733d4c9c3b0d6417f8b3f335de252cc14
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-07-15 19:26:20 -0400

    ayo! more multiline docstrings

--- imperial_py/utils/parser.py
+++ imperial_py/utils/parser.py
@@ -21,7 +21,9 @@ __all__ = (
 
 
 def ensure_json(response: Response) -> dict:
-    """ ensures response from API will be a valid python dictionary """
+    """
+    ensures response from API will be a valid python dictionary
+    """
     try:
         return response.json()
     except JSONDecodeError:
@@ -29,7 +31,9 @@ def ensure_json(response: Response) -> dict:
 
 
 def to_snake_case(json: dict) -> dict:
-    """ replaces camelCased keys with snake_cased keys """
+    """
+    replaces camelCased keys with snake_cased keys
+    """
     json = {(key if key.islower() else snake_regex.sub("_", key).lower()): value for key, value in json.items()}
     # note: in python 3.8+ this can be done with list comprehension with the := operator
     for key, value in json.items():
@@ -39,7 +43,9 @@ def to_snake_case(json: dict) -> dict:
 
 
 def to_camel_case(json: dict) -> dict:
-    """ replaces snake_cased keys with camelCased keys """
+    """
+    replaces snake_cased keys with camelCased keys
+    """
     # please don't make fun of my minified-looking comps
     json = {
         ("".join((word.capitalize() if i != 0 else word.lower()) for i, word in enumerate(key.split("_")))): value
@@ -52,7 +58,9 @@ def to_camel_case(json: dict) -> dict:
 
 
 def parse_dates(json: dict) -> dict:
-    """ replaces unix timestamps with python datetime objects """
+    """
+    replaces unix timestamps with python datetime objects
+    """
     # this just creates a more specific pointer
     document = json.get("document")
     if document and "creation_date" in document and "expiration_date" in document:
@@ -62,6 +70,8 @@ def parse_dates(json: dict) -> dict:
 
 
 def get_date_difference(now: datetime, later: datetime) -> Optional[int]:
-    """ :returns: the number of days between two datetime objects (or None if there are 0 days between) """
+    """
+    :returns: the number of days between two datetime objects (or None if there are 0 days between)
+    """
     days = (later - now).days
     return days if days > 0 else None
