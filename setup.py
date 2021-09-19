863ce7c6d8367d976bdd13c1846ac2bdb38b5bb9
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-09-18 20:54:22 -0400

    bump: `2.0.1`

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt", encoding="utf-8") as req_file:
 
 setup(
     name="imperial.py",
-    version="2.0.0",
+    version="2.0.1",
     description="ImperialBin is a hastebin alternative built with UI and user experience in mind.",
     long_description=readme,
     long_description_content_type="text/markdown",
